fa7632d4bd6071571d9d2939cd9c09882f382b49
Shakibur Rahman <shakiburrahmanwork@gmail.com>
2026-01-29 03:06:57 +0600

    Update imports.js

--- src/components/brand/imports.js
+++ src/components/brand/imports.js
@@ -4,4 +4,5 @@ import atlassian from "../../assets/atlassian.png";
 import dropbox from "../../assets/dropbox.png";
 import shopify from "../../assets/shopify.png";
 
+
 export { google, slack, atlassian, dropbox, shopify };
